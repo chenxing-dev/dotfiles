b310bc50905b968ffab55bc73ca0812adff8fae5
陈刑 <dev-chenxing.io@outlook.com>
2025-07-03 15:43:55 +0800

    Add startpage-alpine as submodule

--- packages/qutebrowser/.config/qutebrowser/config.py
+++ packages/qutebrowser/.config/qutebrowser/config.py
@@ -1,4 +1,5 @@
 # pylint: disable=C0114
+import os
 from qutebrowser.config.configfiles import ConfigAPI
 from qutebrowser.config.config import ConfigContainer
 config = config  # type: ConfigAPI # pylint: disable=E0602,W0127
@@ -6,8 +7,11 @@ c = c  # type: ConfigContainer # pylint: disable=E0602,W0127
 
 # c.fonts.default_family = "'FiraCode Nerd Font Mono'"
 
-c.url.default_page = "~/.config/qutebrowser/startpage/index.html"
-c.url.start_pages = "~/.config/qutebrowser/startpage/index.html"
+startpage_path = os.path.expanduser(
+    "~/.config/qutebrowser/startpage/index.html")
+
+c.url.start_pages = [startpage_path]
+c.url.default_page = startpage_path
 
 c.url.searchengines = {
     'DEFAULT': 'https://searx.foobar.vip/search?q={}',
