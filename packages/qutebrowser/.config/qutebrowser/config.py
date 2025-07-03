a0d59ec4dd7a34dc310aa1db211cb85be43ee00d
chenxing-dev <dev.chenxing@gmail.com>
2025-07-03 22:57:46 +0800

    qutebrowser: post-stow

--- packages/qutebrowser/.config/qutebrowser/config.py
+++ packages/qutebrowser/.config/qutebrowser/config.py
@@ -2,17 +2,24 @@
 import os
 from qutebrowser.config.configfiles import ConfigAPI
 from qutebrowser.config.config import ConfigContainer
+
 config = config  # type: ConfigAPI # pylint: disable=E0602,W0127
 c = c  # type: ConfigContainer # pylint: disable=E0602,W0127
 
-# c.fonts.default_family = "'FiraCode Nerd Font Mono'"
+config.load_autoconfig(False)
+
+c.content.javascript.clipboard = "access"  # enable clipboard access
+c.content.local_content_can_access_remote_urls = True
+
+c.fonts.default_family = ["FiraCode Nerd Font Mono", "Noto Color Emoji"]
+c.fonts.default_size = "12pt"
 
 startpage_path = os.path.expanduser(
     "~/.config/qutebrowser/startpage/index.html")
-
 c.url.start_pages = [startpage_path]
 c.url.default_page = startpage_path
-
 c.url.searchengines = {
-    'DEFAULT': 'https://searx.foobar.vip/search?q={}',
+    "DEFAULT": "https://searx.foobar.vip/search?q={}",
 }
+
+c.window.hide_decoration = False
